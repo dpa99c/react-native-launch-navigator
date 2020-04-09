f5c3f862e8be83ebb1c8a8fca5eca33f0df35bbb
Harshal Bhavsar <hb2708@gmail.com>
2020-04-09 17:57:50 +0800

    Fix for Android X

--- android/src/main/java/uk/co/workingedge/RNLaunchNavigator/RNUtils.java
+++ android/src/main/java/uk/co/workingedge/RNLaunchNavigator/RNUtils.java
@@ -1,6 +1,6 @@
 package uk.co.workingedge.RNLaunchNavigator;
 
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
